e0f0499b019965a702a6d690a24d59d3f8bebf94
Asif Nawaz <asif.nawaz@infinikorn.com>
2022-06-10 10:27:54 +0500

    Set up the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
+# Parent controller which all other controllers inherit from.
+#
+# Before actions and methods defined here will be available to
+# all other controllers.
+#
 class ApplicationController < ActionController::Base
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# Handles managing tasks
+#
 class TasksController < ApplicationController
+  #
+  # GET /
+  #
+  def index
+    @tasks = Task.all
+  end
 end

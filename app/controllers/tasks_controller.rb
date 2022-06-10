91645fa7307dd58f9825239631d3990424976761
Asif Nawaz <asif.nawaz@infinikorn.com>
2022-06-10 10:42:04 +0500

    Added action create for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,5 +8,28 @@ class TasksController < ApplicationController
   #
   def index
     @tasks = Task.all
+    @task = Task.new
+  end
+
+  # POST /tasks
+  #
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html do
+          redirect_to tasks_url, notice: 'Task was successfully created'
+        end
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end

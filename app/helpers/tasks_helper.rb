3d0aed71c86737c7c7afed57d50cfff90a1a0a36
Asif Nawaz <asif.nawaz@infinikorn.com>
2022-06-10 10:18:05 +0500

    Added Task model

---
+++ app/helpers/tasks_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Helper for Task
+module TasksHelper
+end
---
+++ db/migrate/20220610051316_create_tasks.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Task Model Migration
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :description, limit: 200
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
